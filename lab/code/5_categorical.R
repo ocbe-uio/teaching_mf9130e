8eaa30f96d173d81294b4f6f73b8cacee504f342
chizhang <andreachizhang@yahoo.com>
2023-04-27 21:08:10 +0200

    categorical

--- lab/code/5_categorical.R
+++ lab/code/5_categorical.R
@@ -6,13 +6,8 @@
 # 1a) ----
 # create variable 'highpef' for pefmean >=500
 
-lung_data <- haven::read_dta('./lab/data/PEFH98-english.dta')
+lung_data <- read.csv('./lab/data/PEFH98-english.csv')
 
-# recode gender: female, male
-lung_data$gender <- factor(lung_data$gender, 
-                           levels = c('1','2'),
-                           labels = c('female','male')) 
-head(lung_data)
 
 
 # examine variable pefmean
@@ -41,10 +36,9 @@ head(highpef_alt)
 
 
 
-# 1b ----
-# association between highpef and gender
-# ?chisq.test
 
+# 1b ----
+# make a table for pefmean>500 vs gender
 # take out gender variable
 gender <- lung_data$gender
 
@@ -58,20 +52,9 @@ table(highpef)
 table_gender_highpef <- table(gender, highpef)
 table_gender_highpef
 
-chisq.test(table_gender_highpef)
 
-# note: 
-# we have not distinguished between exposure and outcome
-# by default, 'table' function puts the first input in rows (gender)
-# the order does not affect the result of chi-square test
-
-# reverse
-table_highpef_gender <- table(highpef, gender)
-chisq.test(table_highpef_gender)
-
-
-
-# extension: compute RR, OR ----
+# 1c ----
+# calculate risk ratio and odds ratio (point, no CI)
 # outcome: high pef (1); reference: 0
 # exposure: gender = male; reference: female 
 
@@ -92,6 +75,26 @@ or <- odds_exposed/odds_unexposed
 or # 73.6
 
 
+# 1d ----
+# association between highpef and gender
+# ?chisq.test
+
+chisq.test(table_gender_highpef)
+
+# note: 
+# we have not distinguished between exposure and outcome
+# by default, 'table' function puts the first input in rows (gender)
+# the order does not affect the result of chi-square test
+
+# reverse
+table_highpef_gender <- table(highpef, gender)
+chisq.test(table_highpef_gender)
+
+
+
+
+
+# optional ----
 # we can verify using packages
 
 # install.packages('epitools')
@@ -121,6 +124,120 @@ epiR::epi.2by2(tb2, method = 'cohort.count')
 # glmmmm <- glm(as.factor(highpef) ~ gender, family = 'binomial')
 # summary(glmmmm)
 # exp(glmmmm$coefficients)
+
+
+
+
+# ____________ ----
+# exercise 2: birth ----
+
+# 2a) ----
+# bwt (continuous) vs smk (categorical)
+
+# load data
+birth <- read.csv('./lab/data/birth.csv')
+head(birth)
+
+# take the variables
+birthweight <- birth$bwt
+smoker <- birth$smk
+
+# get to know how many smoker and non-smoker
+table(smoker)
+
+# histogram for all (both categories together)
+hist(birthweight)
+
+# separate birthweight based on smoking
+bw_smoker <- birthweight[smoker == 'smoker']
+bw_nonsmoker <- birthweight[smoker == 'nonsmoker']
+
+# histogram for smokers
+hist(bw_smoker)
+hist(bw_nonsmoker)
+
+# boxplot 
+boxplot(bw_smoker)
+boxplot(bw_nonsmoker)
+
+# can produce summary statistics too 
+summary(bw_smoker)
+summary(bw_nonsmoker)
+
+
+# 2b) ----
+# check assumption of normality
+
+qqnorm(bw_smoker)
+qqline(bw_smoker)
+
+qqnorm(bw_nonsmoker)
+qqline(bw_nonsmoker)
+
+# two samples t-test 
+t.test(bw_smoker, bw_nonsmoker, paired = F) # welch
+t.test(bw_smoker, bw_nonsmoker, paired = F, var.equal = T)
+
+
+
+# 2c) ----
+# hypertension vs low birth weight
+
+# take out the variables
+head(birth)
+hypertension <- birth$ht
+
+
+# separate birthweight based on hypertension
+bw_hypertension <- birthweight[hypertension == 'yes']
+bw_nohypertension <- birthweight[hypertension == 'no']
+
+hist(bw_hypertension)
+hist(bw_nohypertension)
+
+# boxplot 
+boxplot(bw_hypertension)
+boxplot(bw_nohypertension)
+
+# can produce summary statistics too 
+summary(bw_hypertension)
+summary(bw_nohypertension)
+
+
+# check normality
+qqnorm(bw_hypertension)
+qqline(bw_hypertension)
+
+qqnorm(bw_nohypertension)
+qqline(bw_nohypertension)
+
+# two samples t-test 
+t.test(bw_hypertension, bw_nohypertension, paired = F) # welch
+t.test(bw_hypertension, bw_nohypertension, paired = F, var.equal = T)
+
+sd(bw_hypertension)
+sd(bw_nohypertension)
+
+
+
+# 2d) ----
+# smoking on birth weight
+# distinguish case/non case, exposed/unexposed!
+low_bw <- birth$low
+table(low_bw)
+
+tb_bw_smk <- table(low_bw, smoker)
+chisq.test(tb_bw_smk)
+
+# hypertension
+
+tb_bw_ht <- table(low_bw, hypertension)
+chisq.test(tb_bw_ht)
+
+# can specify the computation: 
+chisq.test(tb_bw_ht, correct = F) # remove the continuity correction
+chisq.test(tb_bw_ht, simulate.p.value = T) # simulate p values
+
 
 
 
